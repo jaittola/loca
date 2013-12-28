54dcb09c25513417914de37e7cd3f23c6ffc4a71
Jukka Aittola <jaittola@iki.fi>
2013-12-28 06:08:42 +0200

    Depth gradient: Remove printing

--- flaska/flaska/static/depth_gradient.js
+++ flaska/flaska/static/depth_gradient.js
@@ -88,9 +88,6 @@ define(['./hsv2rgb'], function(hsv2rgb) {
                                                                 endHue2),
                                                     sat, value));
             }
-
-            for (var j = 0; j < maxDepth; ++j)
-                console.log(depthGradient[j]);
         };
 
         var depthLegendId = function(cell) {
